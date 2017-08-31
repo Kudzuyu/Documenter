b4b5a7e46ee4ccc6cb14c37a5728590340aeaecf
Kudzuyu <sake4280@gmail.com>
2017-08-31 12:09:46 +0900

    Title, split function
    * Make title screeen
    * split move function to show(), erase(), and move()

--- main.py
+++ main.py
@@ -4,51 +4,62 @@
 from bearlibterminal import terminal
 import numpy as np
 
-WINWIDTH = 80   # ウィンドウの幅
-WINHEIGHT = 40  # ウィンドウの高さ。
+WINWIDTH = 100   # ウィンドウの幅
+WINHEIGHT = 30  # ウィンドウの高さ。
+
+MAPWIDTH = 40
+MAPHEIGHT = 20
+FONT = "font: font/M+_NerdMono_Win.ttf"
 
 Map = np.zeros((WINWIDTH, WINHEIGHT))
 terminal.open()
-terminal.set("font: Ricty-Regular.ttf, size=12")
-terminal.set("window:size=" + str(WINWIDTH) + "x" + str(WINHEIGHT))
+
+
+terminal.set("window:size=" + str(WINWIDTH) + "x" + str(WINHEIGHT) +
+             ", title='Documenter'")
+terminal.set(FONT + ", size=8x16")
 
 
 class Player():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def show(self):
         terminal.layer(2)
-        terminal.printf(y, x, "@")
+        terminal.printf(self.x, self.y, "@")
+        terminal.layer(0)
+        terminal.refresh()
+
+    def erase(self):
+        terminal.layer(2)
+        terminal.printf(self.x, self.y, " ")
         terminal.layer(0)
         terminal.refresh()
 
     def moveright(self):
-        if self.x < WINWIDTH - 1 and self.ismovable(self.x + 1, self.y):
-            terminal.printf(self.x, self.y, " ")
+        if self.x < MAPWIDTH - 1 and self.ismovable(self.x + 1, self.y):
+            self.erase()
             self.x += 1
-            terminal.printf(self.x, self.y, "@")
-            terminal.refresh()
+            self.show()
 
     def moveleft(self):
         if self.x > 0 and self.ismovable(self.x - 1, self.y):
-            terminal.printf(self.x, self.y, " ")
+            self.erase()
             self.x -= 1
-            terminal.printf(self.x, self.y, "@")
-            terminal.refresh()
+            self.show()
 
     def moveup(self):
         if self.y > 0 and self.ismovable(self.x, self.y - 1):
-            terminal.printf(self.x, self.y, " ")
+            self.erase()
             self.y -= 1
-            terminal.printf(self.x, self.y, "@")
-            terminal.refresh()
+            self.show()
 
     def movedown(self):
-        if self.y < WINHEIGHT - 1 and self.ismovable(self.x, self.y + 1):
-            terminal.printf(self.x, self.y, " ")
+        if self.y < MAPHEIGHT - 1 and self.ismovable(self.x, self.y + 1):
+            self.erase()
             self.y += 1
-            terminal.printf(self.x, self.y, "@")
-            terminal.refresh()
+            self.show()
 
     def ismovable(self, x, y):
         if Map[x][y] == 0:
@@ -73,8 +84,8 @@ class mapobject():
 
     def generete(self, num):
         for i in range(num):
-            randx = np.random.randint(WINWIDTH)
-            randy = np.random.randint(WINHEIGHT)
+            randx = np.random.randint(MAPWIDTH)
+            randy = np.random.randint(MAPHEIGHT)
             Map[randx][randy] = 1
             terminal.printf(randx, randy, self.ch)
         terminal.refresh()
@@ -83,7 +94,7 @@ class mapobject():
 def Coord(obj):
     terminal.layer(0)
     terminal.bkcolor("black")
-    terminal.printf(0, WINHEIGHT - 1, '{0:2d},{1:2d}'.format(obj.x, obj.y))
+    terminal.printf(0, MAPHEIGHT - 1, '{0:2d},{1:2d}'.format(obj.x, obj.y))
     terminal.refresh()
 
 
@@ -92,23 +103,82 @@ def game(obj):
     obj.generete(13, 13, 17, 17, '*')
 
 
-wall = mapobject('#')
+def pause():
+    pass
+
+
+def settings():
+    terminal.clear()
+    terminal.printf(10, 0, "Settigns")
+    terminal.refresh()
+    while True:
+        key = terminal.read()
+        if key == terminal.TK_ESCAPE:
+            break
+
+
+def title():
+    options = [[15, "Play"], [45, "Settings"], [80, "Quit"]]
+    selected = 0
+    terminal.clear()
+    terminal.set("title font: font/Erika_Ormig.ttf, size=160x320, spacing=6x1")
+    terminal.printf(18, 6, "[font=title]Documenter")
+    while True:
+        selected = selected % len(options)
+        for i in range(len(options)):
+            if i == selected:
+                terminal.color("orange")
+            else:
+                terminal.color("white")
+            terminal.printf(options[i][0], 20, options[i][1])
+        terminal.refresh()
+        key = terminal.read()
+        if key == terminal.TK_CLOSE:
+            terminal.close()
+            return 'Quit'
+        elif key == terminal.TK_RIGHT:
+            selected += 1
+        elif key == terminal.TK_LEFT:
+            selected -= 1
+
+        elif key == terminal.TK_ENTER:
+            return options[selected][1]
+
+
+def main():
+    terminal.clear()
+    wall = mapobject('#')
+    wall.generete(40)
+    Coord(pl)
+    while True:
+        key = terminal.read()
+        if key == terminal.TK_CLOSE:
+            terminal.close()
+            return 'Quit'
+        elif key == terminal.TK_ESCAPE:
+            pause()
+        terminal.layer(2)
+        terminal.color("red")
+        pl.move(key)
+        terminal.color("white")
+        terminal.layer(0)
+
+
+def title_game_loop():
+    while True:
+        opr = title()
+        if opr == 'Quit':
+            break
+        elif opr == 'Settings':
+            settings()
+        elif opr == 'Play':
+            play = main()
+            if play == 'Quit':
+                break
+
+
 pl = Player(0, 0)
 
-terminal.bkcolor("gray")
-wall.generete(4)
-
-while True:
-    Coord(pl)
-    key = terminal.read()
-    if key == terminal.TK_CLOSE:
-        break
-
-    terminal.layer(2)
-    terminal.color("red")
-    pl.move(key)
-    terminal.color("white")
-    terminal.layer(0)
-
-terminal.close()
+if __name__ == "__main__":
+    title_game_loop()
 
